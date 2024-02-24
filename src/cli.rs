abd65ac48ab35f989469a5a98353e8b2e2593e6e
qgriffith <qgriffith@gmail.com>
2024-02-24 10:23:19 -0500

    A bit of refactoring on the arguments and now checks to see if a flag is passed to sync to strava. More clean up to come

--- src/cli.rs
+++ src/cli.rs
@@ -1,16 +1,60 @@
-use clap::{arg, Command};
+use clap::{value_parser, Arg, ArgAction, Command, ArgMatches};
 
-pub(crate) fn cli() -> Command {
+/// Creates the last argument for weigh-in.
+///
+/// The `last` argument is used to specify the weigh-in date relative to the current day.
+/// It can be set to `1` for the current day, `2` for the previous day, and so on.
+///
+/// # Returns
+/// Returns an instance of `Arg` struct that represents the `last` argument.
+fn create_last_arg() -> Arg {
+    Arg::new("last")
+        .short('l')
+        .long("last")
+        .help("Last weigh-in. Set to 1 for current day, 2 for the previous etc...")
+        .action(ArgAction::Set)
+        .default_missing_value("1")
+        .num_args(0..=1)
+        .value_parser(value_parser!(i64))
+}
+
+/// Creates a command line argument for enabling weight synchronization with Strava.
+///
+/// The `-s` or `--strava-sync` option can be used to enable weight synchronization with Strava.
+/// By default, weight synchronization is disabled.
+///
+/// The `strava_sync_enabled` variable will be `true` if the `-s` or `--strava-sync` option is provided.
+///
+/// # Returns
+///
+/// The `Arg` struct representing the `--strava-sync` command line option.
+fn create_strava_sync_arg() -> Arg {
+    Arg::new("strava-sync")
+        .long("strava-sync")
+        .short('s')
+        .help("Sync weight to strava")
+        .action(ArgAction::SetTrue)
+        .required(false)
+}
+
+/// Returns a `Command` object for the `fitness-connect` CLI.
+///
+/// # Return
+///
+/// Returns a `Command` object for the `fitness-connect` CLI.
+pub(crate) fn cli() -> ArgMatches {
     Command::new("fitness-connect")
         .about("A sync tool for various fitness apps")
+        .version("0.1.0")
         .subcommand_required(true)
         .arg_required_else_help(true)
-        .allow_external_subcommands(true)
+        // Withings subcommand
         .subcommand(
             Command::new("withings")
+                .short_flag('W')
+                .long_flag("withings")
                 .about("Get Data from Withings")
-                .arg(arg!(<LAST> "Integer: Day to get weight. 1 eq current day, 2 the day prior etc...").value_parser(clap::value_parser!(i64)))
-                .arg(arg!(<STRAVA> "Sync to Strava")
-                )
-        )
+                .arg(create_last_arg())
+                .arg(create_strava_sync_arg()),
+        ).get_matches()
 }
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,30 @@
 mod modules;
 mod cli;
 
-use crate::modules::strava::update_athlete_weight;
 use crate::modules::withings::{get_day_before_timestamp, get_weight_by_date};
 use std::process::exit;
 use crate::cli::cli;
+use crate::modules::strava::update_athlete_weight;
 
-fn process_cli() -> i64 {
-    let matches = cli().get_matches();
-    match matches.subcommand() {
-        Some(("withings", sub_matches)) => {
-           let last = sub_matches.get_one::<i64>("LAST").expect("required");
-           last.to_owned()
+fn process_cli()  {
+    match cli().subcommand() {
+        Some(("withings", withing_matches )) => {
+            if withing_matches.contains_id("last") {
+                let last = withing_matches.get_one::<i64>("last").expect("contains_id");
+                println!("Getting weight for the day {}\n", last);
+                let weight_in_kgs = get_and_format_weight(last.to_owned());
+
+                if withing_matches.get_flag("strava-sync") {
+                    let strava_sync = withing_matches.get_one::<bool>("strava-sync").expect("is present");
+                    println!("Syncing to Strava...\n");
+                    let weight = weight_in_kgs.unwrap();
+                    update_athlete_weight(weight.as_str());
+                    println!("Weight updated in Strava to {} kg\n", weight)
+                }
+            }
         }
         _ => unreachable!()
     }
-
 }
 fn get_and_format_weight(last: i64) -> Option<String> {
     match get_weight_by_date(get_day_before_timestamp(last)) {
@@ -29,8 +38,4 @@ fn get_and_format_weight(last: i64) -> Option<String> {
 
 fn main() {
     let last = process_cli();
-    if let Some(weight_in_kgs) = get_and_format_weight(last) {
-        update_athlete_weight(&weight_in_kgs);
-        println!("Weight updated in Strava to {} kg", weight_in_kgs)
-    }
 }
